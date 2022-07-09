6ecdb616be7cfe473c8a1137740d6452f47e3054
Naf <nnegussie7@gmail.com>
2022-07-09 12:44:01 -0400

    Confirm pg and google map api

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// *Google Map API
+//src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCa0K7K6LvUmWowJv5xcYDggiKUTfo0m8U&callback=initMap&v=weekly"
+//defer
+//load function initMap
+//function initMap() {
+    //const options = {
+        //zoom: 8,
+        //center:{lat:39.7684,lng:-86.1581}
+    //}
+    //const map = new google.map.Map(document.getElementById("map"), {
+        
+    //});
+    //marker postion
+    //const marker = new google.maps.Marker({
+       // position: uluru,
+       // map: map,
+   // });
+}
