93e817b0bc8100659bdee1abc37eda72557a23bd
Naf <nnegussie7@gmail.com>
2022-07-11 23:07:15 -0400

    google api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,7 @@
-// *Google Map API
-//src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCa0K7K6LvUmWowJv5xcYDggiKUTfo0m8U&callback=initMap&v=weekly"
-//defer
-//load function initMap
-//function initMap() {
-    //const options = {
-        //zoom: 8,
-        //center:{lat:39.7684,lng:-86.1581}
-    //}
-    //const map = new google.map.Map(document.getElementById("map"), {
-        
-    //});
-    //marker postion
-    //const marker = new google.maps.Marker({
-       // position: uluru,
-       // map: map,
-   // });
-}
-
-//load map
-function initMap() {
-   var uluru = {lat: 39.7684, lng: -86.1581 },
-   var map = new google.map.Map(doctument.getElementById(`map`), {
-      zoom: 8,
-      mapId: `4cde6798d5472110`,
-   });
-    
-var marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-});
-}
-window.initMap = initMap;
+function myMap() {
+    var mapProp = {
+      center: new google.maps.LatLng(39.7684, -86.1581),
+      zoom: 5,
+    };
+    var map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
+  }
