836ff42f3cb0830dc203c803af9c90e5b59406d1
Naf <nnegussie7@gmail.com>
2022-07-18 21:05:07 -0400

    code cleanup

--- public/assets/js/confirm.js
+++ public/assets/js/confirm.js
@@ -35,26 +35,7 @@ $(document).ready(function() {
 
 });
 
-//map
-//map referance
-// let map = null;
-// function initMap() {
-//     let location = new Object();
-//     navigator.geolocation.getCurrentPosition(function (pos) {
-//         location.lat = pos.coords.latitude;
-//         location.long = pos.coords.longitude;
-//         //var location = { lat: 39.7684, 86.1581};
-//         map = new google.maps.Map(document.getElementById("map"), {
-//             zoom: 8,
-//             center: { lat: 39.7684, lng: -86.1581 }
-//         });
 
-//         var marker = new google.maps.Marker({
-//             position: { lat: 39.7684, lng: -86.1581 },
-//             map: map
-//         });
-//     }
-// }
 function initMap(){
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 37.422, lng: -122.084},
@@ -69,19 +50,17 @@ function initMap(){
 
 function mapsSelector() {
     if (
-      /* if we're on iOS, open in Apple Maps */
       navigator.platform.indexOf("iPhone") != -1 ||
       navigator.platform.indexOf("iPad") != -1 ||
       navigator.platform.indexOf("iPod") != -1
     )
       window.open(
-        "http://maps.apple.com/?q=Broccoli+Bill's&sll=40.00537,-86.08867&z=10&t=s"
+        "http://maps.apple.com/?q=Indianapolis's&sll=37.422,-122.084&z=10&t=s"
       );
-    /* else use Google */
-    // window.open("https://maps.google.com/maps?daddr=40.00537,-86.08867&amp;ll=");
+ 
     else
       window.open(
-        "https://maps.google.com/maps/place/Broccoli+Bill's/@40.005404,-86.0910243,17z/data=!3m1!4b1!4m5!3m4!1s0x8814b1fcb424599f:0xba2e242f8178eb65!8m2!3d40.0053999!4d-86.0888356"
+        "https://maps.google.com/maps/place/indianapolis/@37.422,-122.084,17z/"
       );
   }
   console.log(initMap)
